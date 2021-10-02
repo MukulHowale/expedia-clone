c7f2d61c97a325d97f9f7721f31058b94f75c9b6
Mukul <howalemukul@gmail.com>
2021-10-02 17:00:59 +0530

    thank you message completed

---
+++ components/thankYouComponent.js
@@ -0,0 +1,32 @@
+let thankYouComponent = () =>{
+    return `
+    <div class="confirmMsg">
+        <div class="loader">
+
+        </div>
+        <div class="myDiv animate-bottom">
+        <div class="confetti">
+    <div class="confetti-piece"></div>
+    <div class="confetti-piece"></div>
+    <div class="confetti-piece"></div>
+    <div class="confetti-piece"></div>
+    <div class="confetti-piece"></div>
+    <div class="confetti-piece"></div>
+    <div class="confetti-piece"></div>
+    <div class="confetti-piece"></div>
+    <div class="confetti-piece"></div>
+    <div class="confetti-piece"></div>
+    <div class="confetti-piece"></div>
+    <div class="confetti-piece"></div>
+    <div class="confetti-piece"></div>
+</div>
+            <img src="https://www.expedia.co.in/_dms/header/logo.svg?locale=en_GB&siteid=27&2" alt="">
+            <h3>Thank you for booking with Expedia!</h3>
+            <h2>Your booking is confirmed.</h2>
+        </div>
+    </div>
+    `
+}
+
+
+export default thankYouComponent;
