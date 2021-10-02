ec3f044821834e4eb4617413799f7b7d282d4178
Sandhya <sandhyamaram9119@gmail.com>
2021-10-02 09:22:42 +0530

    checkout page added

---
+++ JavaScripts/priceCheckout.js
@@ -0,0 +1,18 @@
+
+let year_div = document.getElementsByClassName("year");
+Array.from(year_div).forEach(div => {
+    for(let i=1900; i<=2021; i++){
+    let opt = document.createElement("option");
+    opt.innerText = i;
+    div.append(opt);
+    }
+});
+let card = document.getElementById("code");
+let code = document.getElementById("security");
+code.addEventListener("focusin",() =>{
+    card.style.display = "block";
+});
+
+code.addEventListener("focusout",() =>{
+    card.style.display = "none";
+});
